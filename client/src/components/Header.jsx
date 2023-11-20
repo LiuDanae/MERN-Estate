c976f2716372f7ae854e32cb5025042a88a6be79
Dan Liu <jump-tiger@outlook.com>
2023-11-20 11:23:10 +0000

    Add header components
    Signed-off-by: Dan Liu <jump-tiger@outlook.com>

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+  return ( <header>
+<h1>
+  <span className='text-slate-500'>
+Sahand
+  </span>
+  <span className='text-slate-700'>
+    Estate
+  </span>
+</h1>
+  </header>)
+ 
+}
